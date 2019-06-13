d64fde44ebb2fd27b4c9381efe301261c14b41ad
Eyal Toledano <eutait@gmail.com>
2019-06-13 19:35:55 -0400

    Initial commit, sets up gems, layout, navigation, flash error area. Ready to begin.

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,20 @@
+require './config/environment'
+require 'rack-flash'
+
+class ApplicationController < Sinatra::Base
+  use Rack::Flash
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "d333ff-d1g1t4lf0und3r"
+  end
+
+  get "/" do
+    erb :index
+  end
+
+
+
+end
